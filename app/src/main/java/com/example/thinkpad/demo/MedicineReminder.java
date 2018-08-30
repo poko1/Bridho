85e14f33d977e6ca2d1a066f45a5f37855c46740
echo-akash <akash.poddar.0799@gmail.com>
2018-08-30 12:56:51 +0600

    Initial commit

---
+++ app/src/main/java/com/example/thinkpad/demo/MedicineReminder.java
@@ -0,0 +1,13 @@
+package com.example.thinkpad.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MedicineReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_reminder);
+    }
+}
